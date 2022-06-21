9248415cab99a760be9226292c62781eb89c98dd
zahra-moeini <z.sh.moeini1379@gmail.com>
2022-06-21 16:10:24 +0430

    comit21jun

---
+++ 21jun/backend/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express=require('express');
+
+// express app
+const app=express()
+
+// middlewre
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+
+// routes
+
+app.get('/',(req,res)=>{
+    res.json({mssg:'welcome to the app'})
+})
+
+// listen for requests
+app.listen(process.env.PORT,()=>{
+    console.log('listeneing on port',process.env.PORT);
+})
+
+
